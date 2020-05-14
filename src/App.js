efc34787d0fc9c884f41fd4cb736b3f51113056a
Andres Posada Llano <posadallano2@gmail.com>
2020-05-14 15:12:51 -0500

    Send transaction: desktop + mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import styled, { css } from "styled-components";
+import { css } from "styled-components";
 import { createGlobalStyle } from 'styled-components';
 import { maxPhone } from "./utils/media";
 
--- src/components/Public/Title/Title.js
+++ src/components/Public/Title/Title.js
@@ -2,12 +2,13 @@ import styled, { css } from "styled-components";
 import { colors } from '../../../utils/colors';
 import { maxPhone } from "../../../utils/media";
 
-const TitleCta = styled.h1`
+const Title = styled.h1`
   font-size: 30px;
   font-weight: 700;
   text-transform: uppercase;
   color: ${colors.white};
   line-height: 1;
+  text-align: center;
   ${maxPhone(css`
     font-size: 22px;
     line-height: 1.1;
@@ -16,4 +17,4 @@ const TitleCta = styled.h1`
   `)};
 `;
 
-export default TitleCta;
+export default Title;
