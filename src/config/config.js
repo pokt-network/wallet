0cafa8eee86c911d41f1d7c31a697b739466cdd3
derrandz <ouaghad.hamza@gmail.com>
2021-05-19 03:03:35 +0100

    feat: validate gateway base url

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,47 @@
+const validators = {
+  GATEWAY_BASE_URL: (value, config) => {
+    const isEmpty = value === "";
+
+    if (isEmpty) {
+      throw new Error("Required configuration environment variable GATEWAY_BASE_URL, received none");
+    }
+
+    const isLocalhostUrl = value.includes("localhost");
+
+    if (isLocalhostUrl) {
+      return value;
+    }
+
+    const isOfficialDomainUrl = value.includes("gateway.pokt.network");
+    const hasChainIdSubdomain = !value.includes("http://gateway.pokt") && !value.includes("https://gateway.pokt");
+
+    if (isOfficialDomainUrl && !hasChainIdSubdomain) {
+      console.warn("Expecting configuration environment variable GATEWAY_BASE_URL to respect the following format: `https://{CHAIN_ID}.{GATEWAY_DOMAIN}`, but received invalid URL");
+      console.warn("Constructing proper URL...");
+
+      const gatewayDomainUrl = value.split("://")[1];
+      console.log(gatewayDomainUrl);
+      const constructedGatewayUrl = `https://${config.CHAIN_ID}.${gatewayDomainUrl}`;
+
+      console.log("constructed:", constructedGatewayUrl);
+
+      return constructedGatewayUrl;
+    }
+
+    return value;
+  },
+  CHAIN_ID: (value) => {
+    const isEmpty = value === "";
+
+    if (isEmpty) {
+      throw new Error("Required configuration environment variable CHAIN_ID, received none");
+    }
+
+    return value;
+  }
+}
+
+
 const configEnvVars = [
   'REACT_APP_CLIENT_PASSPHRASE',
   'REACT_APP_CLIENT_PUBLIC_KEY',
@@ -22,7 +66,7 @@ const configEnvVars = [
   'REACT_APP_MAX_TRANSACTION_LIST_COUNT',
   'REACT_APP_TX_FEE',
   'REACT_APP_SESSION_LENGTH',
-  'REACT_APP_GATEWAY_URL',
+  'REACT_APP_GATEWAY_BASE_URL',
   'REACT_APP_HTTP_TIMEOUT',
   'REACT_APP_HTTP_HEADERS',
   'REACT_APP_USE_LEGACY_CODEC'
@@ -39,9 +83,21 @@ const loadEnvFromList = (list) => list.reduce(
  * For now the config is literraly = env variables
  * keep this piece of code if we want to do config gymnastics
  * */
-const loadConfig = () => {
-  const env = loadEnvFromList(configEnvVars);
-  return env;
+const loadConfigFromEnv = () => {
+  const configObj = loadEnvFromList(configEnvVars);
+
+  // order of values matters!
+  const validatedEnvVars = ['CHAIN_ID', 'GATEWAY_BASE_URL']
+
+  validatedEnvVars.forEach(
+    (envVarKey) => {
+      console.log({ envVarKey, envVarVal: configObj[envVarKey] })
+      const v = validators[envVarKey](configObj[envVarKey], configObj); // as long as it does not through we are good
+      configObj[envVarKey] = v;
+    }
+  );
+
+  return configObj;
 }
 
-export const Config = loadConfig();
+export const Config = loadConfigFromEnv();
--- src/datasource/index.js
+++ src/datasource/index.js
@@ -3,7 +3,7 @@ import {Config} from "../config/config";
 import { DataSource } from "./datasource"
 
 const dataSourceConfig = {
-  gatewayUrl: Config.GATEWAY_URL,
+  gatewayUrl: Config.GATEWAY_BASE_URL,
   http: {
     timeout: Config.HTTP_TIMEOUT,
     headers: Config.HTTP_HEADERS,
