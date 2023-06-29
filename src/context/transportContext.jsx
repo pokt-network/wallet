6695ef34ae5815f3491b5f3d21eb03ce59a271ff
PseudoMera <albinesteban@outlook.com>
2023-06-29 15:07:34 -0400

    chore: logs cleanup

--- src/context/transportContext.jsx
+++ src/context/transportContext.jsx
@@ -247,12 +247,10 @@ export function TransportProvider({ children }) {
     amount,
     outputAddress
   ) => {
-    console.log("TRANSPORT CONTEXT");
     setIsHardwareWalletLoading(true);
     const entropy = Number(
       Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
     ).toString();
-    console.log("entropy: ", entropy);
 
     const tx = {
       chain_id: Config.CHAIN_ID,
@@ -283,18 +281,13 @@ export function TransportProvider({ children }) {
       },
     };
 
-    console.log("tx: ", tx);
-
     try {
       const stringifiedTx = JSON.stringify(tx);
-      console.log("string tx: ", stringifiedTx);
       const hexTx = Buffer.from(stringifiedTx, "utf-8").toString("hex");
-      console.log("hexTx: ", hexTx);
       const sig = await pocketApp.signTransaction(
         LEDGER_CONFIG.derivationPath,
         hexTx
       );
-      console.log("sig: ", sig);
       const ledgerTxResponse = await dataSource.stakeNodeFromLedger(
         publicKey,
         sig.signature,
@@ -302,7 +295,6 @@ export function TransportProvider({ children }) {
         tx
       );
       if (typeGuard(ledgerTxResponse, Error)) {
-        console.log("TRANSPORT LEDGER TX RES ERROR: ", ledgerTxResponse);
         setIsHardwareWalletLoading(false);
         return ledgerTxResponse;
       }
